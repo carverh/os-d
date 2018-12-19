d57368f6ed6d31abe05b49a7b32fa39db683eb9e
Carver Harrison <ch@dorper.me>
2018-12-19 11:29:38 -0800

    Update screen.d

--- drivers/screen.d
+++ drivers/screen.d
@@ -2,33 +2,33 @@ module drivers.screen;
 
 import drivers.vbe;
 
-struct screen {
+struct Screen {
     uint w, h;
     uint x, y;
     uint pitch;
     uint* framebuffer;
 
-    this(vbe_mode* mode) {
+    this(VbeMode* mode) {
         this.w = mode.width;
         this.h = mode.height;
         this.pitch = mode.pitch;
         this.framebuffer = cast(uint*)mode.framebuffer;
     }
 
-    void put_pixel(uint x, uint y, uint colour) {
+    void putPixel(uint x, uint y, uint colour) {
         this.framebuffer[y * w + x] = colour;
     }
 
-    void draw_char(char c, int x, int y, int fg, int bg) {
+    void drawChar(char c, int x, int y, int fg, int bg) {
         extern(C) extern __gshared ushort* cp437;
         int orig_x = x;
 
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 8; j++) {
                 if((cp437[c * 16 + i] >> (7 - j)) & 1) {
-                    this.put_pixel(x++, y, fg);
+                    this.putPixel(x++, y, fg);
                 } else {
-                    this.put_pixel(x++, y, bg);
+                    this.putPixel(x++, y, bg);
                 }
             }
             y++;
